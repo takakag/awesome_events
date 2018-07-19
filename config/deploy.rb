b310cbcf6c1635b16cc28d47badc3f997bdfcb9f
takashi k <takashi0718k@gmail.com>
2018-07-19 21:59:01 +0900

    complete

--- config/deploy.rb
+++ config/deploy.rb
@@ -85,7 +85,7 @@ namespace :deploy do
   task :upload do
     on roles(:app) do |host|
       if test "[ !-d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
+        execute "mkdir -p #{shared_path}/config/"
       end
       upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
       upload!('config/database.yml', "#{shared_path}/config/database.yml")
